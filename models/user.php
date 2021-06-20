d592133e4aa2716df7fb501fd8f8abba39f1c2aa
thezline <juan.bajonero.gdcan@gmail.com>
2021-06-19 18:44:21 -0700

    Model user created

--- models/user.php
+++ models/user.php
@@ -3,3 +3,28 @@
 /**
  *   User model
  */
+
+include_once("./db/database.php");
+
+class User_Model extends Connection {
+    public $db;
+
+    public function __construct() {
+        $this->db = Connection::conn();
+    }
+
+    public function save($name, $last_name, $email, $password) {
+        $passwordEncrypted = password_hash($password, PASSWORD_DEFAULT);
+
+        $query = "INSERT INTO users(name, last_name, email, password) 
+            values(:name, :last_name, :email, :password)";
+
+        $sql = $this->db->prepare($query);
+        $sql->bindParam(':name', $name, PDO::PARAM_STR, 30);
+        $sql->bindParam(':last_name', $last_name, PDO::PARAM_STR, 30);
+        $sql->bindParam(':email', $email, PDO::PARAM_STR, 100);
+        $sql->bindParam(':password', $passwordEncrypted, PDO::PARAM_STR, 100);
+
+        $sql->execute();
+    }
+}
